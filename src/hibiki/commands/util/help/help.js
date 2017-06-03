a9a373bcbfb70cbf82aa28394cf57c4860e65612
sr229 <aguilarralph13@gmail.com>
2017-06-03 18:04:37 +0800

    eslint

--- src/hibiki/commands/util/help/help.js
+++ src/hibiki/commands/util/help/help.js
@@ -4,9 +4,11 @@ class Help extends Command {
     constructor(client) {
         super(client);
     }
-
+/** @todo remove disable once there's an actual help command here */
+/* eslint-disable no-unused-vars */
     run(ctx) {
-        return new Promise((resolve, reject) => {
+        return new Promise((resolve, reject) => { 
+            
             // put your commands all under here
         });
     }
