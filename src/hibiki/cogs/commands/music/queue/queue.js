2feaf9b2dae5108c787d40038922e8577e13c4ef
noud02 <noud.kerver@hotmail.nl>
2017-07-09 07:54:43 +0200

    add temp queue command

--- src/hibiki/cogs/commands/music/queue/queue.js
+++ src/hibiki/cogs/commands/music/queue/queue.js
@@ -8,7 +8,13 @@ class Queue extends Command {
 
     run(ctx) {
         return new Promise((resolve, reject) => {
-
+            if (ctx.db.guild.queue.length > 0) resolve({
+                embed: {
+                    title: 'Queue',
+                    description: ctx.db.guild.queue.map((s, i) => `${i}. ${s}`);
+                }
+            });
+            else resolve('The queue is empty!')
         });
     }
 }
