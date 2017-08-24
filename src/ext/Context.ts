d193e66401c117500dada5d8b746c72968e86dd2
noud02 <noud.kerver@hotmail.nl>
2017-08-24 20:58:57 +0200

    add ts eval to ctx

--- src/ext/Context.ts
+++ src/ext/Context.ts
@@ -2,7 +2,13 @@
 
 import * as Eris from "eris";
 import * as minimist from "minimist";
+import * as ts from "typescript";
+import * as vm from "vm";
 import { Shard } from "../client/Shard";
+
+export interface IEvalContext extends vm.Context {
+    [key: string]: any;
+}
 
 export class Context {
 
@@ -24,6 +30,58 @@ export class Context {
 
     public sendCode (type: string, ...code: any[]): Promise<Eris.Message> {
         return this.msg.channel.createMessage(`\`\`\`${type}\n${code.join(" ")}\`\`\``);
+    }
+
+    public async eval (code: string, customProps: { [key: string]: any }): Promise<any> {
+        const builtins: string[] = require("repl")._builtinLibs;
+        const context: IEvalContext = vm.createContext();
+
+        for (const builtin of builtins) {
+            Object.defineProperty(context, builtin, {
+                get() {
+                    context[builtin] = require(builtin);
+
+                    return require(builtin);
+                },
+                set(val: any) {
+                    delete context[builtin];
+                    context[builtin] = val;
+                },
+                configurable: true,
+            });
+        }
+
+        for (const prop of Object.keys(customProps)) {
+            Object.defineProperty(context, prop, {
+                get() {
+                    context[prop] = customProps[prop];
+
+                    return customProps[prop];
+                },
+                set(val: any) {
+                    delete context[prop];
+                    context[prop] = val;
+                },
+                configurable: true,
+            });
+        }
+        const transpiled: ts.TranspileOutput = ts.transpileModule(code, {
+            compilerOptions: {
+                target: ts.ScriptTarget.ES2016,
+                libs: ["es6"],
+                noImplicitAny: true,
+                noUnusedParameters: true,
+                experimentalDecorators: true,
+                emitDecoratorMetadata: true,
+                allowJs: false,
+                strict: true,
+                noFallthroughCasesInSwitch: true,
+                module: ts.ModuleKind.CommonJS,
+                moduleResolution: ts.ModuleResolutionKind.NodeJs,
+            },
+        });
+
+        return vm.runInContext(transpiled.outputText, context);
     }
 
 }
