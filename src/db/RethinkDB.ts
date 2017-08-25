92d7fda9cf156fae5b876c752876a4273f663508
noud02 <noud.kerver@hotmail.nl>
2017-08-25 07:21:31 +0200

    am fine with sql

--- src/db/RethinkDB.ts
+++
@@ -1,9 +0,0 @@
-// RethinkDB.ts - RethinkDB implementation if you don't want SQL (Capuccino@ClaraIO)
-
-/** @todo complete this */
-import * as rt from "rethinkdbdash";
-
-export class RethinkDB {
-    public client : rt = newrt (this.options);
-    constructor( private options: rt.options) {};
-}
