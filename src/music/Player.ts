c76a93a118414facf95848d6d0290c5fc26ba4d5
noud02 <noud.kerver@hotmail.nl>
2017-09-13 17:03:24 +0200

    add more things

--- src/music/Player.ts
+++ src/music/Player.ts
@@ -2,29 +2,65 @@
 
 import * as Eris from "eris";
 import { EventEmitter } from "events";
+import { Shard } from "../client/Shard";
 import { Queue } from "./Queue";
 import { Track } from "./Track";
 
 export class Player extends EventEmitter {
 
     public queue: Queue = new Queue();
+    public id: string = this.guild.id;
+    public connection: Eris.VoiceConnection | undefined = this.shard.voiceConnections.get(this.id);
+    public downloading: boolean = false;
 
-    constructor (public guild: Eris.Guild) {
+    constructor (private shard: Shard, public guild: Eris.Guild, public channel: Eris.GuildChannel, public voice: Eris.GuildChannel) {
         super();
-    }
 
-    public get current (): Track {
-        return this.queue.current;
-    }
+        this.queue.on("next", (track: Track) => this.play(track));
 
-    public async play (channel: Eris.GuildChannel, voice: Eris.GuildChannel): Promise<Track> {
         if (channel.type !== 0) {
-            return Promise.reject(new Error("Channel is not a text channel"));
+            throw new Error("Channel is not a text channel");
         }
 
         if (voice.type !== 2) {
+            throw new Error("Voicechannel is not a voice channel");
+        }
+    }
+
+    public get current (): Track | undefined {
+        return this.queue.current;
+    }
+
+    public join (channel: Eris.GuildChannel): Promise<Eris.VoiceConnection> {
+        if (channel.type !== 2) {
             return Promise.reject(new Error("Voicechannel is not a voice channel"));
         }
+
+        return this.shard.joinVoiceChannel(channel.id);
+    }
+
+    public async play (track: Track): Promise<Track | undefined> {
+        if (!this.connection) {
+            return Promise.reject(new Error("Not connected!"));
+        }
+
+        if (this.connection.connecting) {
+            return Promise.reject(new Error("Still connecting!"));
+        }
+
+        if (this.connection.paused) {
+            return Promise.reject(new Error("Music is paused!"));
+        }
+
+        if (this.connection.playing) {
+            return Promise.reject(new Error("Already playing music!"));
+        }
+
+        if (this.downloading) {
+            return Promise.reject(new Error("Still downloading music!"));
+        }
+
+        this.connection.play(track.download(), {});
 
         return Promise.resolve(this.queue.next());
     }
